c43b3fce8c0ce12a25358e7ec9c23dac8cee747a
mbarzegary <mbarzegary@msn.com>
2022-07-18 18:51:39 +0200

    change graph scale in degradation plot script

--- plot_scripts/cup/plot_degradation.py
+++ plot_scripts/cup/plot_degradation.py
@@ -2,12 +2,12 @@ from helpers import *
 
 # fig = plt.figure(figsize=(10,6))
 # ax = fig.add_axes([0.1,0.1,0.5,0.8])
-fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(8,6))
+fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(6,4))
 
 time, rate_high, rate_low = load_from_file("degradation.csv", "\t", 1)
 
-plot_fig(ax, time, rate_high, 's', 'blue', 'Degradation in saline solution', 1.0, 'black', 5)
-plot_fig(ax, time, rate_low, 'o', 'red', 'Degradation in buffered solution', 1.0, 'black', 5)
+plot_fig(ax, time, rate_high, 's', 'blue', 'Saline solution', 1.0, 'black', 5)
+plot_fig(ax, time, rate_low, 'o', 'red', 'Buffered solution', 1.0, 'black', 5)
 ax.set_title("Biodegradation over time")
 
 ax.set_ylim(0, 80)
@@ -16,7 +16,7 @@ ax.set_ylabel("Mass loss (%)")
 ax.set_xlabel("Time (hour)")
 # axes.yaxis.set_major_locator(MultipleLocator(50))
 # axes.yaxis.set_minor_locator(MultipleLocator(50))
-ax.legend(loc="upper left")
+ax.legend(loc="center right")
 
 plt.tight_layout()
 plt.savefig('degradation_rate.png', dpi=300)
